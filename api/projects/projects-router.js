a3653b4f43ee04731cb685dd7a208156fd07455d
EstrellaDionis <EstrellaDionis@gmail.com>
2022-01-13 22:46:50 -0500

    added base routes to projects-router.js

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,32 @@
 // Write your "projects" router here!
+const express = require('express');
+
+
+const router = express.Router();
+
+router.get('/api/projects', (req, res) => {
+    res.send(200).json({
+        status: 200, 
+        message: '',
+    })
+})
+
+router.get('api/projects/:id', (req, res) => {
+
+})
+
+router.post('/api/projects', (req, res) => {
+
+})
+
+router.put('/api/projects/:id', (req, res) => {
+
+})
+
+router.delete('/api/projects/id', (req, res) => {
+
+})
+
+router.get('/api/projects/:id/actions', (req, res) => {
+    
+})
